001b24d5c79ac513b2a3899def22007af2816ebd
unknown <eugeneamo85@gmail.com>
2023-04-04 14:44:04 +0000

    basic dependency injection structure done

--- src/main/kotlin/com/config/DependencyInjection.kt
+++ src/main/kotlin/com/config/DependencyInjection.kt
@@ -1,2 +1,18 @@
 package com.config
 
+import com.components.chatRoom.repo.ChatRoomDAOImpl
+import com.components.message.repo.DirectMessageDAOImpl
+import com.components.message.repo.RoomMessageDAOImpl
+import com.components.user.repo.UserDAOImpl
+import io.ktor.server.application.*
+import org.kodein.di.DI
+import org.kodein.di.bindSingleton
+
+fun Application.configureDI(){
+    DI{
+        bindSingleton { UserDAOImpl()}
+        bindSingleton { ChatRoomDAOImpl()}
+        bindSingleton { DirectMessageDAOImpl()}
+        bindSingleton { RoomMessageDAOImpl()}
+    }
+}
